ba30b3bc64d2bac70075fc2cf6786b59a48c4883
Elda-Dani <eldadani2001@gmail.com>
2022-10-13 14:57:57 +0530

    form

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -9,6 +9,25 @@ export class AddcourseComponent implements OnInit {
 
   constructor() { }
 
+  courseTitle=""
+  courseDescription=""
+  courseDuration=""
+  courseDate=""
+  courseVenue=""
+
+
+  readValues=()=>{
+let data={
+ " courseTitle":this.courseTitle,
+  "courseDescription":this.courseDescription,
+ "courseDuration":this.courseDuration,
+ " courseDate":this.courseDate,
+  "courseVenue":this.courseVenue
+}
+
+console.log(data)
+  }
+
   ngOnInit(): void {
   }
 
