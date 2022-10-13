eb7099915105ce08aac6ea42bdf2ed6e1af93560
Elda-Dani <eldadani2001@gmail.com>
2022-10-13 16:53:34 +0530

    s

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-addcourse',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddcourseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private myapi:ApiService) { }
 
   courseTitle=""
   courseDescription=""
@@ -18,14 +19,20 @@ export class AddcourseComponent implements OnInit {
 
   readValues=()=>{
 let data={
- " courseTitle":this.courseTitle,
+ "courseTitle":this.courseTitle,
   "courseDescription":this.courseDescription,
  "courseDuration":this.courseDuration,
- " courseDate":this.courseDate,
+ "courseDate":this.courseDate,
   "courseVenue":this.courseVenue
 }
 
 console.log(data)
+this.myapi.addCourse(data).subscribe(
+  (response)=>{
+    console.log(response) 
+    alert("Successfully added")
+  }
+)
   }
 
   ngOnInit(): void {
